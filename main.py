512feb1ffe418738a5979a5e93a18386b8a9ba7c
Jayaprakash8887 <Jayaprakash.narayanaswamy@tarento.com>
2024-02-27 15:27:38 +0530

    "all apis integration"

--- main.py
+++ main.py
@@ -274,7 +274,7 @@ async def fetch_content(request: GetContentRequest) -> GetContentResponse:
     if language_code_list is None:
         raise HTTPException(status_code=422, detail="supported_lang_codes not configured!")
 
-    language = request.language.strip().lower()
+    language = request.language
     if language is None or language == "" or language not in language_code_list:
         raise HTTPException(status_code=422, detail="Unsupported language code entered!")
 
