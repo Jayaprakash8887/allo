f89a72e28972ef81095c0d382fa8d7c6e62575bb
Jayaprakash8887 <Jayaprakash.narayanaswamy@tarento.com>
2024-03-11 10:58:59 +0530

    Local LLM Integration

--- main.py
+++ main.py
@@ -4,20 +4,21 @@ import string
 from datetime import datetime
 from typing import Optional
 
-import openai
+import requests
+# import openai
 import redis
 from dotenv import load_dotenv
 from fastapi import FastAPI, status, Header
 from fastapi.middleware.cors import CORSMiddleware
-from langchain.agents import AgentExecutor
-from langchain.agents import OpenAIFunctionsAgent
-from langchain.chat_models.openai import ChatOpenAI
-from langchain.memory import RedisChatMessageHistory
-from langchain.prompts import MessagesPlaceholder, HumanMessagePromptTemplate
+# from langchain.agents import AgentExecutor
+# from langchain.agents import OpenAIFunctionsAgent
+# from langchain.chat_models.openai import ChatOpenAI
+# from langchain.memory import RedisChatMessageHistory
+# from langchain.prompts import MessagesPlaceholder, HumanMessagePromptTemplate
 from langchain.pydantic_v1 import BaseModel, Field
-from langchain.schema.messages import (
-    SystemMessage,
-)
+# from langchain.schema.messages import (
+#     SystemMessage,
+# )
 from langchain.tools import StructuredTool
 
 from io_processing import *
@@ -25,13 +26,13 @@ from logger import logger
 from utils import is_url, is_base64
 
 gpt_model = get_config_value("llm", "gpt_model", None)
-
+llm_base_path = get_config_value("llm", "llm_base_path", None)
 welcome_prompt = get_config_value("llm", "all_chat_prompt", None)
 feedback_prompt = get_config_value("llm", "feedback_prompt", None)
 nudge_user_prompt = get_config_value("llm", "nudge_user_prompt", None)
 closure_prompt = get_config_value("llm", "closure_prompt", None)
 
-llm_client = openai.OpenAI(api_key=os.environ["OPENAI_API_KEY"])
+# llm_client = openai.OpenAI(api_key=os.environ["OPENAI_API_KEY"])
 
 app = FastAPI(title="ALL BOT Service",
               #   docs_url=None,  # Swagger UI: disable it by setting docs_url=None
@@ -65,6 +66,10 @@ def store_data(key, value):
 def retrieve_data(key):
     data_from_redis = redis_client.get(key)
     return data_from_redis.decode('utf-8') if data_from_redis is not None else None
+
+
+def clear_data(key):
+    redis_client.delete(key)
 
 
 @app.on_event("startup")
@@ -177,15 +182,27 @@ def capture_user_emotions(user_id: str, user_feedback: str, emotion_category: st
     if not next_message:
         logger.debug(f"closure_prompt: {closure_prompt}")
         if closure_prompt:
-            closure_res = llm_client.chat.completions.create(
-                model=gpt_model,
-                messages=[
-                    {"role": "system", "content": closure_prompt},
-                    {"role": "user", "content": user_session_emotions}
-                ],
+            # closure_res = llm_client.chat.completions.create(
+            #     model=gpt_model,
+            #     messages=[
+            #         {"role": "system", "content": closure_prompt},
+            #         {"role": "user", "content": user_session_emotions}
+            #     ],
+            # )
+            # closure_res_message = closure_res.choices[0].message.model_dump()
+            # closure_message = closure_res_message["content"]
+
+            messages = [
+                {"role": "system", "content": closure_prompt},
+                {"role": "user", "content": user_session_emotions}
+            ]
+            closure_res = requests.post(
+                llm_base_path,
+                json={"model": gpt_model, "messages": messages, "stream": True},
             )
-            closure_res_message = closure_res.choices[0].message.model_dump()
-            closure_message = closure_res_message["content"]
+            closure_res_message = closure_res.json()
+            closure_message = closure_res_message["response"]
+
             logger.inf({"user_id": user_id, "language": language, "session_id": session_id, "closure_message": closure_message})
             next_message = closure_message
 
@@ -238,7 +255,9 @@ def get_health() -> HealthCheck:
     return HealthCheck(status="OK")
 
 
-llm = ChatOpenAI(model=gpt_model, temperature=0)
+# llm = ChatOpenAI(model=gpt_model, temperature=0)
+
+# llm = Ollama(base_url='http://152.67.162.156:11434', model="mistral")
 
 
 @app.post("/v1/learn_language", include_in_schema=True)
@@ -491,8 +510,8 @@ async def submit_response(request: UserAnswerRequest) -> GetContentResponse:
     else:
         output_response = get_showcase_content(user_id, language, current_session_id)
 
-    conversation_text = None
-    conversation_audio = None
+    # conversation_text = None
+    # conversation_audio = None
 
     logger.info({"user_id": user_id, "output_response": output_response})
 
@@ -508,17 +527,32 @@ async def submit_response(request: UserAnswerRequest) -> GetContentResponse:
         logger.debug(f"nudge_user_prompt: {nudge_user_prompt}")
         nudge_message = None
         if nudge_user_prompt:
-            nudge_res = llm_client.chat.completions.create(
-                model=gpt_model,
-                messages=[
-                    {"role": "system", "content": nudge_user_prompt}
-                ],
+            # nudge_res = llm_client.chat.completions.create(
+            #     model=gpt_model,
+            #     messages=[
+            #         {"role": "system", "content": nudge_user_prompt}
+            #     ],
+            # )
+            # nudge_res_message = nudge_res.choices[0].message.model_dump()
+            # nudge_message = nudge_res_message["content"]
+
+            messages = [{"role": "system", "content": nudge_user_prompt}]
+            nudge_res = requests.post(
+                llm_base_path,
+                json={"model": gpt_model, "messages": messages, "stream": True},
             )
-            nudge_res_message = nudge_res.choices[0].message.model_dump()
-            nudge_message = nudge_res_message["content"]
+            nudge_res_message = nudge_res.json()
+            nudge_message = nudge_res_message["response"]
             logger.info({"user_id": user_id, "user_language": language, "nudge_message": nudge_message})
 
         conversation_audio, conversation_text = process_outgoing_voice_manual(nudge_message, language)
+        conversation_response = ConversationResponse(audio=conversation_audio, text=conversation_text)
+
+        if output_response.audio is not None:
+            content_response = GetContentResponse(conversation=conversation_response, content=output_response)
+        else:
+            content_response = GetContentResponse(conversation=conversation_response)
+        return content_response
     else:
         store_data(user_id + "_" + language + "_" + current_session_id + "_completed", "true")
         return invoke_llm_feedback(user_id, language, current_session_id, 'user_completed')  # match language:
@@ -529,59 +563,73 @@ async def submit_response(request: UserAnswerRequest) -> GetContentResponse:
         #         conversation_text = "You have completed the assessment! Re-login to start fresh."
         #         conversation_audio = "https://ax2cel5zyviy.compat.objectstorage.ap-hyderabad-1.oraclecloud.com/sbdjb-kathaasaagara/audio-output-20240228-065008.mp3"
 
-    conversation_response = ConversationResponse(audio=conversation_audio, text=conversation_text)
-
-    if output_response.audio is not None:
-        content_response = GetContentResponse(conversation=conversation_response, content=output_response)
-    else:
-        content_response = GetContentResponse(conversation=conversation_response)
-    return content_response
+    # conversation_response = ConversationResponse(audio=conversation_audio, text=conversation_text)
+    #
+    # if output_response.audio is not None:
+    #     content_response = GetContentResponse(conversation=conversation_response, content=output_response)
+    # else:
+    #     content_response = GetContentResponse(conversation=conversation_response)
+    # return content_response
 
 
 def invoke_llm(user_id, language, current_session_id, user_input) -> GetContentResponse:
     # setup Redis as a message store
-    message_history = RedisChatMessageHistory(
-        url="redis://" + redis_host + ":" + redis_port + "/" + redis_index, session_id=current_session_id
-    )
+    # message_history = RedisChatMessageHistory(
+    #     url="redis://" + redis_host + ":" + redis_port + "/" + redis_index, session_id=current_session_id
+    # )
 
-    tools = [voice_maker]
+    # tools = [voice_maker]
+    # print("welcome_prompt:: ", welcome_prompt)
+    # prompt = OpenAIFunctionsAgent.create_prompt(
+    #     system_message=SystemMessage(
+    #         content=welcome_prompt,
+    #     ),
+    #     extra_prompt_messages=[
+    #         MessagesPlaceholder(variable_name='chat_history'),
+    #         HumanMessagePromptTemplate.from_template("user: {input}, language: {input_language}, history: {chat_history}"),
+    #
+    #         MessagesPlaceholder(variable_name="agent_scratchpad")
+    #     ]
+    # )
+    # agent = OpenAIFunctionsAgent(llm=llm, tools=tools, prompt=prompt)
+    # agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+    # llm_response = agent_executor.invoke({"input": user_input, "chat_history": message_history.messages, "input_language": language})
 
-    print("welcome_prompt:: ", welcome_prompt)
+    chat_history_messages = retrieve_data(current_session_id + "_welcome")
 
-    prompt = OpenAIFunctionsAgent.create_prompt(
-        system_message=SystemMessage(
-            content=welcome_prompt,
-        ),
-        extra_prompt_messages=[
-            MessagesPlaceholder(variable_name='chat_history'),
-            HumanMessagePromptTemplate.from_template("user: {input}, language: {input_language}, history: {chat_history}"),
-
-            MessagesPlaceholder(variable_name="agent_scratchpad")
+    if chat_history_messages:
+        chat_history_messages = json.loads(chat_history_messages)
+        messages = [
+            {"role": "system", "content": welcome_prompt},
+            chat_history_messages,
+            {"role": "user", "content": user_input}
         ]
+    else:
+        chat_history_messages = []
+        messages = [
+            {"role": "system", "content": welcome_prompt},
+            {"role": "user", "content": user_input}
+        ]
+
+    logger.info({"user_id": user_id, "user_language": language, "messages": messages})
+
+    welcome_res = requests.post(
+        llm_base_path,
+        json={"model": gpt_model, "messages": messages, "stream": True},
     )
+    welcome_res_message = welcome_res.json()
+    logger.info({"user_id": user_id, "user_language": language, "llm response": welcome_res_message})
+    ai_assistant = welcome_res_message["response"]
+    logger.info({"user_id": user_id, "user_language": language, "ai_assistant": ai_assistant})
 
-    agent = OpenAIFunctionsAgent(llm=llm, tools=tools, prompt=prompt)
+    chat_history_messages.append({"role": "user", "content": user_input})
+    chat_history_messages.append({"role": "assistant", "content": ai_assistant})
+    store_data(current_session_id + "_welcome", json.dumps(chat_history_messages))
 
-    agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
-
-    llm_response = agent_executor.invoke({"input": user_input, "chat_history": message_history.messages, "input_language": language})
-
-    logger.info({"llm response": llm_response})
-
-    message_history.add_user_message(user_input)
-
-    try:
-        ai_assistant = llm_response["output"]["eng_text"]
-        message_history.add_ai_message(ai_assistant)
-        audio_output_url = llm_response["output"]["audio"]
-        ai_reg_text = llm_response["output"]["reg_text"]
-    except:
-        ai_assistant = llm_response["output"]
-        message_history.add_ai_message(ai_assistant)
-        audio_output_url, ai_reg_text = process_outgoing_voice_manual(ai_assistant, language)
+    audio_output_url, ai_reg_text = process_outgoing_voice_manual(ai_assistant, language)
 
     if ai_assistant.startswith("Goodbye") and ai_assistant.endswith("See you soon!"):
-        message_history.clear()
+        clear_data(current_session_id + "_welcome")
 
     if ai_assistant == "user_agreed" or ai_assistant == "'user_agreed'":
         content_response = func_get_content(user_id, language)
@@ -591,63 +639,110 @@ def invoke_llm(user_id, language, current_session_id, user_input) -> GetContentR
         conversation_response = ConversationResponse(audio=conversation_audio, text=conversation_text)
         content_response = GetContentResponse(conversation=conversation_response)
 
-    logger.info({"content_response": content_response})
+    logger.info({"user_id": user_id, "user_language": language, "content_response": content_response})
     return content_response
 
 
+# def invoke_llm_feedback_old(user_id, language, current_session_id, user_input) -> GetContentResponse:
+#     # setup Redis as a message store
+#     feedback_message_history = RedisChatMessageHistory(
+#         url="redis://" + redis_host + ":" + redis_port + "/" + redis_index, session_id=current_session_id + "_feedback"
+#     )
+#
+#     feedback_tools = [store_user_emotions]
+#
+#     # print("feedback_prompt:: ", feedback_prompt)
+#
+#     user_fb_prompt = OpenAIFunctionsAgent.create_prompt(
+#         system_message=SystemMessage(
+#             content=feedback_prompt,
+#         ),
+#         extra_prompt_messages=[
+#             MessagesPlaceholder(variable_name='chat_history'),
+#             HumanMessagePromptTemplate.from_template("user: {input}, language: {input_language}, history: {chat_history}, user_id: {user_id}, session_id: {session_id}"),
+#             MessagesPlaceholder(variable_name="agent_scratchpad")
+#         ]
+#     )
+#
+#     fb_agent = OpenAIFunctionsAgent(llm=llm, tools=feedback_tools, prompt=user_fb_prompt)
+#
+#     fb_agent_executor = AgentExecutor(agent=fb_agent, tools=feedback_tools, verbose=True)
+#
+#     logger.info({"input": user_input, "chat_history": feedback_message_history.messages, "input_language": language, "user_id": user_id, "session_id": current_session_id})
+#
+#     fb_llm_response = fb_agent_executor.invoke({"input": user_input, "chat_history": feedback_message_history.messages, "input_language": language, "user_id": user_id, "session_id": current_session_id})
+#
+#     logger.info({"feedback llm response": fb_llm_response})
+#
+#     feedback_message_history.add_user_message(user_input)
+#
+#     try:
+#         fb_ai_assistant = fb_llm_response["output"]["eng_text"]
+#         feedback_message_history.add_ai_message(fb_ai_assistant)
+#         audio_output_url = fb_llm_response["output"]["audio"]
+#         ai_reg_text = fb_llm_response["output"]["reg_text"]
+#     except:
+#         fb_ai_assistant = fb_llm_response["output"]
+#         feedback_message_history.add_ai_message(fb_ai_assistant)
+#         audio_output_url, ai_reg_text = process_outgoing_voice_manual(fb_ai_assistant, language)
+#
+#     if fb_ai_assistant.startswith("Goodbye") and fb_ai_assistant.endswith("See you soon!"):
+#         feedback_message_history.clear()
+#
+#     conversation_text = ai_reg_text
+#     conversation_audio = audio_output_url
+#     conversation_response = ConversationResponse(audio=conversation_audio, text=conversation_text)
+#     content_response = GetContentResponse(conversation=conversation_response)
+#
+#     logger.info({"feedback content_response": content_response})
+#     return content_response
+
 def invoke_llm_feedback(user_id, language, current_session_id, user_input) -> GetContentResponse:
-    # setup Redis as a message store
-    feedback_message_history = RedisChatMessageHistory(
-        url="redis://" + redis_host + ":" + redis_port + "/" + redis_index, session_id=current_session_id + "_feedback"
-    )
+    chat_history_messages = retrieve_data(current_session_id + "_feedback")
 
-    feedback_tools = [store_user_emotions]
-
-    # print("feedback_prompt:: ", feedback_prompt)
-
-    user_fb_prompt = OpenAIFunctionsAgent.create_prompt(
-        system_message=SystemMessage(
-            content=feedback_prompt,
-        ),
-        extra_prompt_messages=[
-            MessagesPlaceholder(variable_name='chat_history'),
-            HumanMessagePromptTemplate.from_template("user: {input}, language: {input_language}, history: {chat_history}, user_id: {user_id}, session_id: {session_id}"),
-            MessagesPlaceholder(variable_name="agent_scratchpad")
+    if chat_history_messages:
+        chat_history_messages = json.loads(chat_history_messages)
+        messages = [
+            {"role": "system", "content": feedback_prompt},
+            chat_history_messages,
+            {"role": "user", "content": user_input}
         ]
+    else:
+        chat_history_messages = []
+        messages = [
+            {"role": "system", "content": feedback_prompt},
+            {"role": "user", "content": user_input}
+        ]
+
+    logger.info({"user_id": user_id, "user_language": language, "feedback messages": messages})
+
+    feedback_res = requests.post(
+        llm_base_path,
+        json={"model": gpt_model, "messages": messages, "stream": True},
     )
+    feedback_res_message = feedback_res.json()
+    logger.info({"user_id": user_id, "user_language": language, "feedback llm response": feedback_res_message})
+    ai_assistant = feedback_res_message["response"]
+    logger.info({"user_id": user_id, "user_language": language, "feedback ai_assistant": ai_assistant})
 
-    fb_agent = OpenAIFunctionsAgent(llm=llm, tools=feedback_tools, prompt=user_fb_prompt)
+    chat_history_messages.append({"role": "user", "content": user_input})
+    chat_history_messages.append({"role": "assistant", "content": ai_assistant})
+    store_data(current_session_id + "_feedback", json.dumps(chat_history_messages))
 
-    fb_agent_executor = AgentExecutor(agent=fb_agent, tools=feedback_tools, verbose=True)
+    audio_output_url, ai_reg_text = process_outgoing_voice_manual(ai_assistant, language)
 
-    logger.info({"input": user_input, "chat_history": feedback_message_history.messages, "input_language": language, "user_id": user_id, "session_id": current_session_id})
-
-    fb_llm_response = fb_agent_executor.invoke({"input": user_input, "chat_history": feedback_message_history.messages, "input_language": language, "user_id": user_id, "session_id": current_session_id})
-
-    logger.info({"feedback llm response": fb_llm_response})
-
-    feedback_message_history.add_user_message(user_input)
-
-    try:
-        fb_ai_assistant = fb_llm_response["output"]["eng_text"]
-        feedback_message_history.add_ai_message(fb_ai_assistant)
-        audio_output_url = fb_llm_response["output"]["audio"]
-        ai_reg_text = fb_llm_response["output"]["reg_text"]
-    except:
-        fb_ai_assistant = fb_llm_response["output"]
-        feedback_message_history.add_ai_message(fb_ai_assistant)
-        audio_output_url, ai_reg_text = process_outgoing_voice_manual(fb_ai_assistant, language)
-
-    if fb_ai_assistant.startswith("Goodbye") and fb_ai_assistant.endswith("See you soon!"):
-        feedback_message_history.clear()
+    if ai_assistant.startswith("Goodbye") and ai_assistant.endswith("See you soon!"):
+        clear_data(current_session_id + "_feedback")
 
     conversation_text = ai_reg_text
     conversation_audio = audio_output_url
     conversation_response = ConversationResponse(audio=conversation_audio, text=conversation_text)
     content_response = GetContentResponse(conversation=conversation_response)
 
-    logger.info({"feedback content_response": content_response})
+    logger.info({"user_id": user_id, "user_language": language, "feedback content_response": content_response})
     return content_response
+
+
 
 
 def generate_sub_session_id(length=24):
