09358cbd7ff401cac0cb901dfdf8966e8d0add02
Jayaprakash8887 <Jayaprakash.narayanaswamy@tarento.com>
2024-03-12 16:51:20 +0530

    Local LLM Integration

--- main.py
+++ main.py
@@ -735,16 +735,16 @@ def invoke_llm_feedback(user_id, language, current_session_id, user_input) -> Ge
     if " | " in ai_assistant:
         strip_index = ai_assistant.index(' | ')
         ai_assistant = ai_assistant[:strip_index]
-    # if "message_to_user:" in ai_assistant or "message\\_to\\_user":
-    #     ai_assistant = ai_assistant.replace("\\", "").replace("message_to_user:", "").strip()
-    # if "Assistant:" in ai_assistant:
-    #     ai_assistant = ai_assistant.replace("Assistant:", "").strip()
-    # if "<emotion_category>" in ai_assistant:
-    #     strip_index = ai_assistant.index('<emotion_category>')
-    #     ai_assistant = ai_assistant[:strip_index]
-    # if "emotion_category:" in ai_assistant:
-    #     strip_index = ai_assistant.index('emotion_category:')
-    #     ai_assistant = ai_assistant[:strip_index]
+    if "message_to_user:" in ai_assistant or "message\\_to\\_user":
+        ai_assistant = ai_assistant.replace("\\", "").replace("message_to_user:", "").strip()
+    if "Assistant:" in ai_assistant:
+        ai_assistant = ai_assistant.replace("Assistant:", "").strip()
+    if "<emotion_category>" in ai_assistant:
+        strip_index = ai_assistant.index('<emotion_category>')
+        ai_assistant = ai_assistant[:strip_index]
+    if "emotion_category:" in ai_assistant:
+        strip_index = ai_assistant.index('emotion_category:')
+        ai_assistant = ai_assistant[:strip_index]
     logger.info({"user_id": user_id, "user_language": language, "feedback ai_assistant": ai_assistant})
 
     chat_history_messages.append({"role": "user", "content": user_input})
